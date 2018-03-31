4c2ec951f2b192fbb1149c855501fbfd4860a6c2
Roman <gotovkoromanhzby@gmail.com>
2018-03-31 16:43:00 +0300

    Update herodrafter.js

--- herodrafter.js
+++ herodrafter.js
@@ -11,7 +11,6 @@ var heroes = ["D.Va","E.T.C.","Абатур","Азмодан","Аларак","А
 var team1 = [];
 var team2 = [];
 var number;
-var map = maps[Math.floor(Math.random() * maps.length)];
 for (i = 0; i < 5; i++){
   	number = Math.floor(Math.random() * heroes.length);
   	team1.push(heroes.splice(number,1));  
@@ -25,8 +24,24 @@ if(team1[4]=="Чо’Галл"){
 if(team2[4]=="Чо’Галл"){
 	team2.shift();
 }
+var olTeam1 = document.getElementById("team1");
+var olTeam2 = document.getElementById("team2");
+var ulMap = document.getElementById("map");
+team1.forEach(function(item, i, arr) {
+  var li = document.createElement('li');
+  li.innerHTML = item;
+	olTeam1.appendChild(li);
+  
+});
 	   
-document.write("<h2>Team 1:</h2>" + team1 + "\n");
-document.write("<h2>Team 2:</h2>" + team2 + "\n");
-document.write("<h2>Map:</h2>" + map);
+team2.forEach(function(item, i, arr) {
+  var li = document.createElement('li');
+  li.innerHTML = item;
+	olTeam2.appendChild(li);
+  
+});
+
+var li = document.createElement('li');
+li.innerHTML = maps[Math.floor(Math.random() * maps.length)];;
+ulMap.appendChild(li);
 }
